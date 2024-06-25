169c834985b05a7f557163404063e9d9f4c5c558
AlefLaniel <alef6940@gmail.com>
2024-06-25 14:36:17 -0300

    update: html de politica de privacidade

--- src/pages/Privacy/index.tsx
+++ src/pages/Privacy/index.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
 
 // import { Container } from './styles';
 
-const Privacy: React.FC = () => {
+const Privacy = () => {
   return (
-    <h1>Politicas e privacidade</h1>
+    <div>
+      <h1>Política de Privacidade</h1>
+      <p>
+        Na Alefdev, privacidade e segurança são prioridades.
+        Comprometemo-nos com a transparência no tratamento dos dados pessoais
+        dos nossos usuários/clientes.
+      </p>
+
+      <h2>1. Quais dados coletamos sobre você e para qual finalidade?</h2>
+      <p>
+        1.1. Dados pessoais fornecidos pelo titular: Não pegamos nenhum tipo de dados pelo usuário
+        <br />
+        1.2. Dados pessoais coletados automaticamente: Também não coletamos nenhum dado pessoal automáticamento no aplicativo
+        <br />
+      </p>
+
+      <h2>2. Como coletamos os seus dados?</h2>
+      <p>
+        2.1. Consentimento: precisamo apenas da permissão do usuário de habilitar o audio para o aplicativo utilizar na reprodução das respostas
+      </p>
+
+      <h2>3. Quais são os seus direitos?</h2>
+      <p>
+        [Informe sobre os direitos do titular, como acesso, retificação,
+        exclusão etc.]
+      </p>
+
+      <h2>4. Como você pode exercer seus direitos de titular?</h2>
+      <p>
+        [Explique como o usuário pode entrar em contato para exercer seus
+        direitos]
+      </p>
+
+      <h2>5. Como e por quanto tempo seus dados serão armazenados?</h2>
+      <p>[Descreva a política de armazenamento dos dados]</p>
+
+      <h2>6. O que fazemos para manter seus dados seguros?</h2>
+      <p>[Explique as medidas de segurança adotadas]</p>
+
+      <h2>7. Com quem seus dados podem ser compartilhados?</h2>
+      <p>7.1. Transferência internacional de dados: [se aplicável]</p>
+
+      <h2>8. Cookies ou dados de navegação</h2>
+      <p>[Explique o uso de cookies e como o usuário pode gerenciá-los]</p>
+
+      <h2>9. Alteração desta Política de Privacidade</h2>
+      <p>[Explique como as alterações serão comunicadas]</p>
+
+      <h2>10. Responsabilidade</h2>
+      <p>
+        10.1. Isenção de responsabilidade: [informe que o usuário deve ler a
+        política completa]
+      </p>
+
+      <h2>11. Encarregado de Proteção de Dados</h2>
+      <p>[Informe o contato do encarregado, se houver]</p>
+    </div>
   );
-}
+};
 
 export default Privacy;
